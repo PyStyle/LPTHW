e8f464d7fb100979a36ed2e4a53de99777219bc1
PyStyle <kyanpur@ws57169.eduroam.haw-hamburg.de>
2016-10-25 12:47:57 +0200

    added info to readme

--- ex14.py
+++ ex14.py
@@ -1,15 +1,18 @@
 from sys import argv
 
 script, user_name = argv
-prompt = ">"
+prompt = ' > '
 
 print "Hi %s, I`m the %s script." % (user_name, script)
-print "Id like to ask you a few questions."
+print "I`d like to ask you a few questions."
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
 
 print "Where do you live %s?" % user_name
 lives = raw_input(prompt)
+
+print "What kind of computer do you have?"
+computer = raw_input(prompt) 
 
 print """
 Alright, so you said %r about liking me.
